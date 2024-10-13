5f9acb04a5b426778d3b781fd0031a2af939647c
Matteo Bernacchi <matteo.bernacchi@gmail.com>
2024-10-13 12:16:23 +0200

    asd

--- terraform/modules/database/firstboot.sh
+++ terraform/modules/database/firstboot.sh
@@ -2,5 +2,5 @@
 dnf makecache
 dnf install git ansible-core -y
 ansible-galaxy collection install amazon.aws
-ansible-pull -U https://github.com/Traktopel/wpov.git -e playbook_dir ansible/playbook.yaml 
+ansible-pull -U https://github.com/Traktopel/wpov.git -e playbook_dir ansible/playbook.yaml -e bucket "${bucket}"
 echo "${bucket}" >> /root/asd.txt
