490c7fb899ad4126c6fedf03d8741cef7e5a223a
Matteo Bernacchi <matteo.bernacchi@gmail.com>
2024-10-13 12:24:51 +0200

    fix# with '#' will be ignored, and an empty message aborts the commit.

--- terraform/modules/database/firstboot.sh
+++ terraform/modules/database/firstboot.sh
@@ -2,5 +2,5 @@
 dnf makecache
 dnf install git ansible-core -y
 ansible-galaxy collection install amazon.aws
-ansible-pull -U https://github.com/Traktopel/wpov.git ansible/playbook.yaml -e bucket "${bucket}"
+ansible-pull -U https://github.com/Traktopel/wpov.git ansible/playbook.yaml -e bucket="${bucket}"
 echo "${bucket}" >> /root/asd.txt
